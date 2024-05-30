819f1206d7a4bbefe128bfe472172580437e24d0
Albertowo <pqdstar_alberto@live.com>
2024-05-29 19:25:43 -0600

    add nuew table product-image

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+import { Product, ProductImage } from './entities/';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
   controllers: [ProductController],
   providers: [ProductService],
 })
